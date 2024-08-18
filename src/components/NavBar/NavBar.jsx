e319e0142b859c5f085d315d6f8136421afddcf2
RomanaKarasenko <143837231+RomanaKarasenko@users.noreply.github.com>
2024-08-18 21:09:18 +0300

    trrr

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
-import css from "./Navbar.module.css";
+import css from "./NavBar.module.css";
 
 const Navbar = () => {
   const location = useLocation();
